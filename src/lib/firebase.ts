6fc19e110146f0d5fecedb01afea8fd86860ab31
RAHAT11790 <rahatsarker224@gmail.com>
2026-02-22 18:41:15 +0600

    Upload firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, push, set, remove, update, query, orderByChild, equalTo, get } from "firebase/database";
+import { getAuth, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP5bfue5FOc0eTO4E52-0A0w3PppO3Mvw",
+  authDomain: "rs-anime.firebaseapp.com",
+  databaseURL: "https://rs-anime-default-rtdb.firebaseio.com",
+  projectId: "rs-anime",
+  storageBucket: "rs-anime.firebasestorage.app",
+  messagingSenderId: "843989457516",
+  appId: "1:843989457516:web:57e0577d092183eedd9649"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export { ref, onValue, push, set, remove, update, query, orderByChild, equalTo, get, signInWithEmailAndPassword, signOut, signInWithPopup };
